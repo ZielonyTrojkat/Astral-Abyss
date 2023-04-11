a7e262246536b1cbc92c8cd420356e004a8e81ed
Zielony Trójkąt <lemonlemongrab1@gmail.com>
2023-04-11 13:33:47 +0200

    content

--- scripts/blocks.js
+++
@@ -1,5 +0,0 @@
-Events.on(ContentInitEvent, e => {
-     Vars.content.block("radiatr-mod-d-dissonance-floor").attributes.set(Attribute.get("dissonance"), 1);
-
-     Vars.content.block("radiatr-mod-d-dissonance-extractor").attribute = Attribute.get("dissonance");
-});
--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,1 @@
 require("attributes");
-require("blocks");
